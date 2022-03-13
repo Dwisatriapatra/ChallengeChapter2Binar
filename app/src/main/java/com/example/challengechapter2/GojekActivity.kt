62ecc18109a30f41d28e203dea981057cb16fd42
Dwi Satria Patra <dwisatriapatra2000@gmail.com>
2022-03-13 21:25:27 +0700

    initial commit

---
+++ app/src/main/java/com/example/challengechapter2/GojekActivity.kt
@@ -0,0 +1,11 @@
+package com.example.challengechapter2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GojekActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gojek)
+    }
+}
